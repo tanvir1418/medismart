b59ded03d61d2193c1cbe46106742d697fc034bd
Tanvir <tanvirahmed1418@gmail.com>
2022-03-24 12:05:08 +0600

    New DB Update

--- database/medismart.sql
+++ database/medismart.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 21, 2022 at 02:22 PM
+-- Generation Time: Mar 24, 2022 at 07:04 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -84,6 +84,7 @@ CREATE TABLE `doctors` (
   `id` int(11) NOT NULL,
   `specilization` varchar(255) DEFAULT NULL,
   `doctorName` varchar(255) DEFAULT NULL,
+  `profile_pic` varchar(255) NOT NULL,
   `docgender` varchar(10) NOT NULL,
   `docdegree` varchar(255) NOT NULL,
   `address` longtext DEFAULT NULL,
@@ -101,17 +102,17 @@ CREATE TABLE `doctors` (
 -- Dumping data for table `doctors`
 --
 
-INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `docgender`, `docdegree`, `address`, `docFees`, `practicedays`, `practicetime`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
-(1, 'Medicine', 'DR. MD. ZAKIR HOSSAIN', 'Male', 'MBBS, FCPS (Medicine)', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '800', 'Saturday - Thursday', '2pm - 5pm', '01778897931', 'zakir@gmail.com', '83f399ed140b122791b15f01b4b3d4e0', '2022-02-20 06:12:39', '2022-03-21 13:06:40'),
-(2, 'Medicine', 'DR. MD. RAFIQUL ISLAM', 'Male', 'FCPS (Medicine), MBBS', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '800', 'Saturday - Thursday', '6pm - 9pm', '01350-897931', 'rafiqul@gmail.com', '3ab9cd9170823691d6734c0a80f82eb3', '2022-02-20 06:17:14', '2022-03-21 13:06:44'),
-(3, 'Nose-Ear-Throat', 'Dr. Hasan Ali', 'Male', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', 'Friday', '6pm - 9pm', '01950-777788', 'hasan@gmail.com', 'f690d3b8d4b51c1f189d886b7bab26b7', '2022-02-20 06:22:12', '2022-03-21 13:06:53'),
-(4, 'Nose-Ear-Throat', 'MD. ABDUL GAFFAR', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', '', '', '01788-897931', 'abdul@gmail.com', '428a78b4fee47253898d7918c0a09160', '2022-02-20 06:25:53', NULL),
-(5, 'Gynecologist', 'DR. SULTANA RAZIA', 'Female', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', 'Saturday - Thursday', '2pm - 5pm', '01333897931', 'sultana@gmail.com', '8c2a804932c40968823558dd3c81dc20', '2022-02-20 06:28:31', '2022-03-21 13:06:47'),
-(6, 'Gynecologist', 'Dr. Mafruha Jahan', 'Female', 'MBBS, FCPS (Gynecologist)', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '500', 'Saturday - Thursday', '10pm - 1am', '01368112255', 'mafruha@gmail.com', 'd083603d9b8a7858c57618ab478437e6', '2022-02-20 06:30:19', '2022-03-21 13:06:29'),
-(7, 'Dentist', 'Dr. Alraji Rumman', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '500', '', '', '01950777736', 'alraji@gmail.com', '1422d9fc12aea8ba6545a63da06db7bd', '2022-02-24 16:00:16', NULL),
-(8, 'Neurology', 'DR. MD. ZAKIRUL ISLAM', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '1000', '', '', '01733897931', 'zakirul@gmail.com', 'bba1de36c5c7b723949fca73f58ed7af', '2022-02-24 16:04:08', NULL),
-(9, 'Cardiologist', 'Dr. Salim Rahman', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '1000', '', '', '01778897939', 'salim@gmail.com', '5c792ab5a27f615dc22f6732e29b1ee6', '2022-02-24 16:06:22', '2022-02-24 16:06:36'),
-(10, 'Cardiologist', 'DR. MD. ARIF RAHMAN', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', '', '', '01778898888', 'arif@gmail.com', 'd53d757c0f838ea49fb46e09cbcc3cb1', '2022-02-24 16:08:19', NULL);
+INSERT INTO `doctors` (`id`, `specilization`, `doctorName`, `profile_pic`, `docgender`, `docdegree`, `address`, `docFees`, `practicedays`, `practicetime`, `contactno`, `docEmail`, `password`, `creationDate`, `updationDate`) VALUES
+(1, 'Medicine', 'DR. MD. ZAKIR HOSSAIN', '1648101106_zakir.jpg', 'Male', 'MBBS, FCPS (Medicine)', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '800', 'Saturday - Thursday', '2pm - 5pm', '01778897931', 'zakir@gmail.com', '83f399ed140b122791b15f01b4b3d4e0', '2022-02-20 06:12:39', '2022-03-24 05:51:46'),
+(2, 'Medicine', 'DR. MD. RAFIQUL ISLAM', '1648101120_rafiq.jpg', 'Male', 'FCPS (Medicine), MBBS', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '800', 'Saturday - Thursday', '6pm - 9pm', '01350897931', 'rafiqul@gmail.com', '3ab9cd9170823691d6734c0a80f82eb3', '2022-02-20 06:17:14', '2022-03-24 05:52:00'),
+(3, 'Nose-Ear-Throat', 'Dr. Hasan Ali', '', 'Male', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', 'Friday', '6pm - 9pm', '01950777788', 'hasan@gmail.com', 'f690d3b8d4b51c1f189d886b7bab26b7', '2022-02-20 06:22:12', '2022-03-24 05:32:53'),
+(4, 'Nose-Ear-Throat', 'MD. ABDUL GAFFAR', '', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', '', '', '01788897931', 'abdul@gmail.com', '428a78b4fee47253898d7918c0a09160', '2022-02-20 06:25:53', '2022-03-24 05:32:57'),
+(5, 'Gynecologist', 'DR. SULTANA RAZIA', '1648101134_rajia.jpg', 'Female', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', 'Saturday - Thursday', '2pm - 5pm', '01333897931', 'sultana@gmail.com', '8c2a804932c40968823558dd3c81dc20', '2022-02-20 06:28:31', '2022-03-24 05:52:14'),
+(6, 'Gynecologist', 'Dr. Mafruha Jahan', '1648101168_mafruha.jpg', 'Female', 'MBBS, FCPS (Gynecologist)', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '500', 'Saturday - Thursday', '10pm - 1am', '01368112255', 'mafruha@gmail.com', 'd083603d9b8a7858c57618ab478437e6', '2022-02-20 06:30:19', '2022-03-24 05:52:48'),
+(7, 'Dentist', 'Dr. Alraji Rumman', '', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '500', '', '', '01950777736', 'alraji@gmail.com', '1422d9fc12aea8ba6545a63da06db7bd', '2022-02-24 16:00:16', NULL),
+(8, 'Neurology', 'DR. MD. ZAKIRUL ISLAM', '', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '1000', '', '', '01733897931', 'zakirul@gmail.com', 'bba1de36c5c7b723949fca73f58ed7af', '2022-02-24 16:04:08', NULL),
+(9, 'Cardiologist', 'Dr. Salim Rahman', '', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '1000', '', '', '01778897939', 'salim@gmail.com', '5c792ab5a27f615dc22f6732e29b1ee6', '2022-02-24 16:06:22', '2022-02-24 16:06:36'),
+(10, 'Cardiologist', 'DR. MD. ARIF RAHMAN', '', '', '', 'Popular Diagnostic Centre Ltd. (Bogra Branch)', '600', '', '', '01778898888', 'arif@gmail.com', 'd53d757c0f838ea49fb46e09cbcc3cb1', '2022-02-24 16:08:19', NULL);
 
 -- --------------------------------------------------------
 
@@ -156,7 +157,12 @@ INSERT INTO `doctorslog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logou
 (20, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-10 10:58:25', '10-03-2022 04:29:23 PM', 1),
 (21, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-10 11:49:06', '10-03-2022 05:27:15 PM', 1),
 (22, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-13 15:47:08', '13-03-2022 09:17:49 PM', 1),
-(23, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-17 07:22:10', '17-03-2022 01:33:18 PM', 1);
+(23, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-17 07:22:10', '17-03-2022 01:33:18 PM', 1),
+(24, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 03:41:41', '24-03-2022 10:07:16 AM', 1),
+(25, 13, 'tanvir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 04:37:32', '24-03-2022 10:07:44 AM', 1),
+(26, 13, 'tanvir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 04:38:22', '24-03-2022 10:17:54 AM', 1),
+(27, 1, 'zakir@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 05:42:46', '24-03-2022 11:18:43 AM', 1),
+(28, NULL, 'admin', 0x3a3a3100000000000000000000000000, '2022-03-24 05:48:53', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -315,7 +321,9 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 (24, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-10 10:26:46', '10-03-2022 03:57:17 PM', 1),
 (25, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-10 11:01:36', '10-03-2022 04:41:25 PM', 1),
 (26, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-13 15:48:02', '13-03-2022 09:19:15 PM', 1),
-(27, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-17 07:13:55', '17-03-2022 12:48:21 PM', 1);
+(27, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-17 07:13:55', '17-03-2022 12:48:21 PM', 1),
+(28, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 05:00:07', '24-03-2022 10:53:26 AM', 1),
+(29, 1, 'afiya@gmail.com', 0x3a3a3100000000000000000000000000, '2022-03-24 05:23:49', '24-03-2022 10:58:12 AM', 1);
 
 -- --------------------------------------------------------
 
@@ -326,6 +334,7 @@ INSERT INTO `userlog` (`id`, `uid`, `username`, `userip`, `loginTime`, `logout`,
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `fullName` varchar(255) DEFAULT NULL,
+  `profile_pic` varchar(255) NOT NULL,
   `address` longtext DEFAULT NULL,
   `city` varchar(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
@@ -339,22 +348,22 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fullName`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
-(1, 'Afiya Humyra', 'Khandar', 'Bogura', 'female', 'afiya@gmail.com', '97d18c1b8a8579f31a6f1ac3b15fb671', '2022-02-20 06:46:03', NULL),
-(2, 'Sahela Rahman', 'Baghopara', 'Bogra', 'female', 'sahela@gmail.com', 'a6b9746d9ff79a5941b4071e8e75ae83', '2022-02-24 14:55:24', '2022-02-24 15:28:42'),
-(3, 'Sadek Islam', 'Shahjahanpur', 'Bogra', 'male', 'sadek@gmail.com', '076c6ec6edcc4f195a113cfbdf7036ea', '2022-02-24 15:00:40', '2022-02-24 15:28:56'),
-(4, 'Rifat Habib', 'Satmatha', 'Bogra', 'male', 'rifat@gmail.com', '2e1e5ce47d4b17e67b3d6b01edc01efc', '2022-02-24 15:03:07', '2022-02-24 15:29:01'),
-(5, 'Nira Fahmida', 'Thengamara', 'Bogra', 'female', 'nira@gmail.com', '52ebe237c42891d907901b721e245230', '2022-02-24 15:04:47', '2022-02-24 15:29:06'),
-(6, 'Azfar Habib', 'Chunarughat', 'Sylhet', 'male', 'azfar@gmail.com', '0d1f613511c190a795a29787c293ec3d', '2022-02-24 15:08:27', NULL),
-(7, 'Lina Tazalla', 'Pirgacha', 'Rangpur', 'female', 'lina@gmail.com', '528a3c500e49e8d14159dd2935ee36a1', '2022-02-24 15:17:27', NULL),
-(8, 'Rashi Rokshana', 'Satmatha', 'Bogra', 'female', 'rashi@gmail.com', '10d0688cd80691eb06fd912793332c46', '2022-02-24 15:18:38', NULL),
-(9, 'Mitu Akter', 'TMSS', 'Bogra', 'female', 'mitu@gmail.com', '9773461275d220d470e42d0113695e36', '2022-02-24 15:19:41', NULL),
-(10, 'Marina Naznin', 'TMSS', 'Bogra', 'female', 'marina@gmail.com', '4bf541f9d353914327dd963461e8677c', '2022-02-24 15:20:34', NULL),
-(11, 'Suhi Akter', 'Satmatha', 'Bogra', 'female', 'suhi@gmail.com', '31217d1e304aeadb5ae682be76729ff3', '2022-02-24 15:21:36', NULL),
-(12, 'Sabrina Sultana', 'TMSS', 'Bogra', 'female', 'sabrina@gmail.com', '67fc4ee9d64a4fee0f99ad4b65a95074', '2022-02-24 15:23:21', NULL),
-(13, 'Asmaul Husna', 'Satmatha', 'Bogra', 'female', 'asmaul@gmail.com', 'ae4650418ffc4ce527361e207bfb23ad', '2022-02-24 15:24:26', NULL),
-(14, 'Mohaiminul Islam', 'Shahjahanpur', 'Bogra', 'male', 'mohaiminul@gmail.com', 'ad6ddfa7d2fcefc4b49cea43b769215d', '2022-02-24 15:25:58', NULL),
-(15, 'Sadiq Islam', 'Kalimondir', 'Bogra', 'male', 'sadiq@gmail.com', '61785a8e996a36a23d1b346e28f5f816', '2022-02-24 15:26:53', NULL);
+INSERT INTO `users` (`id`, `fullName`, `profile_pic`, `address`, `city`, `gender`, `email`, `password`, `regDate`, `updationDate`) VALUES
+(1, 'Afiya Humyra', '1648099395_afiya.jpg', 'Khandar', 'Bogura', 'female', 'afiya@gmail.com', '97d18c1b8a8579f31a6f1ac3b15fb671', '2022-02-20 06:46:03', '2022-03-24 05:23:15'),
+(2, 'Sahela Rahman', '', 'Baghopara', 'Bogra', 'female', 'sahela@gmail.com', 'a6b9746d9ff79a5941b4071e8e75ae83', '2022-02-24 14:55:24', '2022-02-24 15:28:42'),
+(3, 'Sadek Islam', '', 'Shahjahanpur', 'Bogra', 'male', 'sadek@gmail.com', '076c6ec6edcc4f195a113cfbdf7036ea', '2022-02-24 15:00:40', '2022-02-24 15:28:56'),
+(4, 'Rifat Habib', '', 'Satmatha', 'Bogra', 'male', 'rifat@gmail.com', '2e1e5ce47d4b17e67b3d6b01edc01efc', '2022-02-24 15:03:07', '2022-02-24 15:29:01'),
+(5, 'Nira Fahmida', '', 'Thengamara', 'Bogra', 'female', 'nira@gmail.com', '52ebe237c42891d907901b721e245230', '2022-02-24 15:04:47', '2022-02-24 15:29:06'),
+(6, 'Azfar Habib', '', 'Chunarughat', 'Sylhet', 'male', 'azfar@gmail.com', '0d1f613511c190a795a29787c293ec3d', '2022-02-24 15:08:27', NULL),
+(7, 'Lina Tazalla', '', 'Pirgacha', 'Rangpur', 'female', 'lina@gmail.com', '528a3c500e49e8d14159dd2935ee36a1', '2022-02-24 15:17:27', NULL),
+(8, 'Rashi Rokshana', '', 'Satmatha', 'Bogra', 'female', 'rashi@gmail.com', '10d0688cd80691eb06fd912793332c46', '2022-02-24 15:18:38', NULL),
+(9, 'Mitu Akter', '', 'TMSS', 'Bogra', 'female', 'mitu@gmail.com', '9773461275d220d470e42d0113695e36', '2022-02-24 15:19:41', NULL),
+(10, 'Marina Naznin', '', 'TMSS', 'Bogra', 'female', 'marina@gmail.com', '4bf541f9d353914327dd963461e8677c', '2022-02-24 15:20:34', NULL),
+(11, 'Suhi Akter', '', 'Satmatha', 'Bogra', 'female', 'suhi@gmail.com', '31217d1e304aeadb5ae682be76729ff3', '2022-02-24 15:21:36', NULL),
+(12, 'Sabrina Sultana', '', 'TMSS', 'Bogra', 'female', 'sabrina@gmail.com', '67fc4ee9d64a4fee0f99ad4b65a95074', '2022-02-24 15:23:21', NULL),
+(13, 'Asmaul Husna', '', 'Satmatha', 'Bogra', 'female', 'asmaul@gmail.com', 'ae4650418ffc4ce527361e207bfb23ad', '2022-02-24 15:24:26', NULL),
+(14, 'Mohaiminul Islam', '', 'Shahjahanpur', 'Bogra', 'male', 'mohaiminul@gmail.com', 'ad6ddfa7d2fcefc4b49cea43b769215d', '2022-02-24 15:25:58', NULL),
+(15, 'Sadiq Islam', '', 'Kalimondir', 'Bogra', 'male', 'sadiq@gmail.com', '61785a8e996a36a23d1b346e28f5f816', '2022-02-24 15:26:53', NULL);
 
 --
 -- Indexes for dumped tables
@@ -441,13 +450,13 @@ ALTER TABLE `appointment`
 -- AUTO_INCREMENT for table `doctors`
 --
 ALTER TABLE `doctors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `doctorslog`
 --
 ALTER TABLE `doctorslog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `doctorspecilization`
@@ -477,7 +486,7 @@ ALTER TABLE `tblpatient`
 -- AUTO_INCREMENT for table `userlog`
 --
 ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
