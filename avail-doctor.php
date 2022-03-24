b7f56dbf8fba8f867632116090551a5205780421
Tanvir <tanvirahmed1418@gmail.com>
2022-03-24 12:04:05 +0600

    Avail Sidebar Design Fixed

--- avail-doctor.php
+++ avail-doctor.php
@@ -137,20 +137,6 @@
         </div>
         <div class="clear"> </div>
     </div>
-    <div class="clear"> </div>
-    <div class="footer">
-        <div class="wrap">
-            <div class="footer-left">
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-
-                    <li><a href="contact.php">contact</a></li>
-                </ul>
-            </div>
-
-            <div class="clear"> </div>
-        </div>
-    </div>
     <!--end-wrap-->
 </body>
 
